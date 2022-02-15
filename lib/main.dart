b46f01e8a2dbd3c75ca26dc56832d86857b0e3bb
Priangshu Dey Prokhor <deypriangshu@gmail.com>
2022-02-15 11:55:19 +0600

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:car_rental_app/routes.dart';
 import 'package:car_rental_app/screens/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+//import 'package:';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Add this
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyA7ayc948CnRfqcTOkvqo5L_bo3wQO3Vjk",
+        appId: "1:887767991985:android:37b485e404d36a49b30490",
+        authDomain: "Car Rental App.firebaseapp.com",
+        databaseURL: "https://{Car Rental App}.firebaseio.com",
+        messagingSenderId: "887767991985",
+        projectId: "car-rental-app-86fd7",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
